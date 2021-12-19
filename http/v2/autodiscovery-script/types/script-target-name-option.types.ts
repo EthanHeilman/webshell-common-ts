77825adf22b5c665a4d6273a492e5a96d16cc638
tflpd <aflpd@bu.edu>
2021-12-20 00:46:12 +0200

    Adjusted to latest autodiscovery script controller updates

---
+++ http/v2/autodiscovery-script/types/script-target-name-option.types.ts
@@ -0,0 +1,6 @@
+export enum ScriptTargetNameOption {
+     Timestamp = 'Timestamp',
+     DigitalOceanMetadata = 'DigitalOceanMetadata',
+     AwsEc2Metadata = 'AwsEc2Metadata',
+     BashHostName = 'BashHostName'
+ };
