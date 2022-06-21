1128a74e26954a403f524be50e705bae899579ba
Thanasis Filippidis <aflpd@bu.edu>
2022-06-21 18:03:20 +0300

    Adjusted TargetSummary to support DAC status (#79)

--- http/v2/target/types/targetBase.types.ts
+++ http/v2/target/types/targetBase.types.ts
@@ -1,11 +1,12 @@
 import { TargetStatus } from './targetStatus.types';
 import { TargetType } from './target.types';
+import { DynamicAccessConfigStatus } from '../dynamic/types/dynamic-access-config-status.types';
 
 export interface TargetBase
 {
     id: string;
     agentPublicKey: string;
-    status: TargetStatus;
+    status: TargetStatus | DynamicAccessConfigStatus;
     name: string;
     environmentId: string;
     type: TargetType;
