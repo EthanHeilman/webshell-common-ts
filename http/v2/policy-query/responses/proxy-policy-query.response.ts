39c9774cb7b13d539bb981128792fff1ef8ea78b
johncmerfeld <john.merfeld@gmail.com>
2022-11-11 10:59:38 -0500

    support zli

--- http/v2/policy-query/responses/proxy-policy-query.response.ts
+++ http/v2/policy-query/responses/proxy-policy-query.response.ts
@@ -1,4 +1,6 @@
+import { TargetRole } from '../../policy/types/target-role.types';
 export interface ProxyPolicyQueryResponse {
     allowed: boolean;
     allowedPolicies: string[];
+    allowedTargetRoles: TargetRole[];
 }
