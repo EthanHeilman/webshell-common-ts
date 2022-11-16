643e16460462b3a89b6af7908c6fa9600fef48d7
johncmerfeld <john.merfeld@gmail.com>
2022-11-16 13:35:59 -0500

    response type

--- http/v2/certificate/responses/generate-certificate.response.ts
+++ http/v2/certificate/responses/generate-certificate.response.ts
@@ -2,5 +2,12 @@ export interface GenerateCertificateResponse {
     caCert: string
     serverCert?: string
     serverKey?: string
-    agentKeyShard?: string // string represntation of a bigint
+    agentKeyShard?: KeyShard // JSON represntation of a private key... actually TODO: might as well make it a struct
+}
+
+export interface KeyShard {
+    d: string
+    e: string
+    eSplit: string
+    n: string
 }
